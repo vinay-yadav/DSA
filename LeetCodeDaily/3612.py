210e0e88f99df9eb2bfbb2b9c389931a03cf0f95
Vinay Yadav <vinay@Vinays-MacBook-Pro.local>
2026-06-16 22:21:56 +0530

    Day 29

---
+++ LeetCodeDaily/3612.py
@@ -0,0 +1,30 @@
+"""
+Process String with Special Operations I
+"""
+
+
+class Solution:
+    def processStr(self, s: str) -> str:
+        result = ""
+
+        for chr in s:
+            if chr == "*":
+                if result != "":
+                    result = result[:-1]
+            elif chr == "#":
+                result += result
+            elif chr == "%":
+                result = result[::-1]
+            else:
+                result += chr
+
+        return result
+
+
+if __name__ == "__main__":
+    testCases = [("a#b%*", "ba"), ("z*#", "")]
+
+    for idx, (s, expected) in enumerate(testCases):
+        result = Solution().processStr(s)
+        status = "Pass" if result == expected else "Fail"
+        print(f"Input {idx}: [{status} -> {result}]")
