f77fca7359b0b0bfc4bf7c22d806352a29975e4a
Vinay Yadav <yadav.vinay1508@gmail.com>
2026-06-22 22:00:29 +0530

    Day 35

---
+++ LeetCodeDaily/1189.py
@@ -0,0 +1,43 @@
+"""
+Maximum Number of Balloons
+"""
+
+
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        freqDict = {
+            "b": 0,
+            "a": 0,
+            "l": 0,
+            "o": 0,
+            "n": 0,
+        }
+
+        for chr in text:
+            if chr in freqDict:
+                freqDict[chr] += 1
+
+        singleCharFreq = min(freqDict["b"], freqDict["a"], freqDict["n"])
+        doubleCharFreq = min(freqDict["l"], freqDict["o"])
+
+        if doubleCharFreq == singleCharFreq * 2:
+            return singleCharFreq
+        
+        return min(singleCharFreq, doubleCharFreq // 2)
+
+if __name__ == "__main__":
+    testCases = [
+        ("nlaebolko", 1),
+        ("loonbalxballpoon", 2),
+        ("leetcode", 0),
+        (
+            "krhizmmgmcrecekgyljqkldocicziihtgpqwbticmvuyznragqoyrukzopfmjhjjxemsxmrsxuqmnkrzhgvtgdgtykhcglurvppvcwhrhrjoislonvvglhdciilduvuiebmffaagxerjeewmtcwmhmtwlxtvlbocczlrppmpjbpnifqtlninyzjtmazxdbzwxthpvrfulvrspycqcghuopjirzoeuqhetnbrcdakilzmklxwudxxhwilasbjjhhfgghogqoofsufysmcqeilaivtmfziumjloewbkjvaahsaaggteppqyuoylgpbdwqubaalfwcqrjeycjbbpifjbpigjdnnswocusuprydgrtxuaojeriigwumlovafxnpibjopjfqzrwemoinmptxddgcszmfprdrichjeqcvikynzigleaajcysusqasqadjemgnyvmzmbcfrttrzonwafrnedglhpudovigwvpimttiketopkvqw",
+            10,
+        ),
+        ("lloo", 0)
+    ]
+
+    for idx, (*inputs, expected) in enumerate(testCases):
+        result = Solution().maxNumberOfBalloons(*inputs)
+        status = "Pass" if result == expected else "Fail"
+        print(f"Input {idx}: [{status} -> {result}]")
