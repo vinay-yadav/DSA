5947b3fe9346f69614bfca7dcffcc3c7cbd0f383
Vinay Yadav <yadav.vinay1508@gmail.com>
2026-08-19 23:46:32 +0530

    Day 93 Binary Version

--- LeetCodeDaily/1386.py
+++ LeetCodeDaily/1386.py
@@ -7,6 +7,37 @@ from collections import defaultdict
 
 class Solution:
     def maxNumberOfFamilies(self, n: int, reservedSeats: list[list[int]]) -> int:
+        """
+        T.C: O(reservedSeats)
+        S.C: O(reservedSeats)
+        """
+        bookedSeats = defaultdict(int)
+
+        for row, seat in reservedSeats:
+            bookedSeats[row] |= 1 << seat
+
+        group1 = 1 << 2 | 1 << 3 | 1 << 4 | 1 << 5
+        group2 = 1 << 4 | 1 << 5 | 1 << 6 | 1 << 7
+        group3 = 1 << 6 | 1 << 7 | 1 << 8 | 1 << 9
+
+        result = 0
+        for value in bookedSeats.values():
+            isGroup1Available = value & group1 == 0
+            isGroup2Available = value & group2 == 0
+            isGroup3Available = value & group3 == 0
+
+            if isGroup1Available and isGroup3Available:
+                result += 2
+            elif isGroup1Available or isGroup2Available or isGroup3Available:
+                result += 1
+
+        return result + (n - len(bookedSeats)) * 2
+
+    def maxNumberOfFamilies1(self, n: int, reservedSeats: list[list[int]]) -> int:
+        """
+        T.C: O(reservedSeats)
+        S.C: O(reservedSeats)
+        """
         bookedSeats = defaultdict(list)
 
         for row, seat in reservedSeats:
@@ -14,31 +45,27 @@ class Solution:
 
         processedVacantBlocks = 0
         for rowData in bookedSeats.values():
-            processedVacantBlocks += self.getVacantBlocks(rowData)
+            block1 = block2 = block3 = True
+
+            for seat in rowData:
+                if seat == 1 or seat == 10:
+                    continue
+
+                if 2 <= seat <= 5:
+                    block1 = False
+
+                if 4 <= seat <= 7:
+                    block2 = False
+
+                if 6 <= seat <= 9:
+                    block3 = False
+
+            if block1 and block3:
+                processedVacantBlocks += 2
+            elif block1 or block2 or block3:
+                processedVacantBlocks += 1
 
         return processedVacantBlocks + (n - len(bookedSeats)) * 2
-
-    def getVacantBlocks(self, rowData) -> int:
-        block1 = block2 = block3 = True
-
-        for seat in rowData:
-            if seat == 1 or seat == 10:
-                continue
-
-            if 2 <= seat <= 5:
-                block1 = False
-
-            if 4 <= seat <= 7:
-                block2 = False
-
-            if 6 <= seat <= 9:
-                block3 = False
-
-        if block1 and block3:
-            return 2
-        elif block1 or block2 or block3:
-            return 1
-        return 0
 
 
 if __name__ == "__main__":
