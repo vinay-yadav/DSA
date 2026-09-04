03e369b1b56ce54a498d5090354b8b5a877b64ab
Vinay Yadav <yadav.vinay1508@gmail.com>
2026-09-04 23:08:57 +0530

    Day 109

---
+++ LeetCodeDaily/3903.py
@@ -0,0 +1,40 @@
+"""
+Smallest Stable Index I
+"""
+
+
+class Solution:
+    def firstStableIndex(self, nums: list[int], k: int) -> int:
+        n = len(nums)
+
+        leftRangeMax = [-1] * n
+        leftRangeMax[0] = nums[0]
+
+        for i in range(1, n):
+            leftRangeMax[i] = max(leftRangeMax[i - 1], nums[i])
+
+        rightRangeMin = [-1] * n
+        rightRangeMin[-1] = nums[-1]
+
+        for i in range(n - 2, -1, -1):
+            rightRangeMin[i] = min(rightRangeMin[i + 1], nums[i])
+
+        instabilityScore = float("inf")
+        for i in range(n):
+            if leftRangeMax[i] - rightRangeMin[i] <= k:
+                instabilityScore = min(instabilityScore, i)
+
+        return instabilityScore if instabilityScore != float("inf") else -1  # type: ignore
+
+
+if __name__ == "__main__":
+    testCases = [
+        ([5, 0, 1, 4], 3, 3),
+        ([3, 2, 1], 1, -1),
+        ([0], 0, 0),
+    ]
+
+    for idx, (*inputs, expected) in enumerate(testCases):
+        result = Solution().firstStableIndex(*inputs)  # type: ignore
+        status = "Pass" if result == expected else "Fail"
+        print(f"Input {idx}: {{{status} -> {result}}}")
